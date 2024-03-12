53a52c8f2fcde1770a877e6a62016e7f774bb14e
Aditya Upadhye <66748266+Aditya-U7@users.noreply.github.com>
2024-03-12 15:43:49 +0530

    Update binary_to_decimal.cpp
    Added the author name, and formatted its style.

--- binary_to_decimal.cpp
+++ binary_to_decimal.cpp
@@ -1,45 +1,51 @@
+/* Author: Aditya Upadhye
+
+A simple program to convert binary to equivalent decimal.
+
+*/
+
 #include <iostream>
 #include <cmath>
 
 void binary_to_decimal(std::string n)
 {
 
-short leng = n.length() - 1;
-short tc = leng;
-int ans = 0;
+	short leng = n.length() - 1;
+	short tc = leng;
+	int ans = 0;
 
-while (leng >= 0)
-{
+	while (leng >= 0)
+	{
 
-std::cout << n[leng] << std::endl;
+		std::cout << n[leng] << std::endl;
 
-if (n[tc - leng] == '1')
-ans = ans + 1 * std::pow(2, leng);
+		if (n[tc - leng] == '1')
+			ans = ans + 1 * std::pow(2, leng);
 
-leng--;
+		leng--;
 
-} 
+	} 
 
-std::cout << ans << std::endl;
+	std::cout << ans << std::endl;
 
 }
 
 
 /*
-void binary_to_octal(std::string n)
-{
+   void binary_to_octal(std::string n)
+   {
 
 
-}
-*/
+   }
+   */
 
 int main()
 {
-std::string user_input;
-std::cin >> user_input;
+	std::string user_input;
+	std::cin >> user_input;
 
-binary_to_decimal(user_input);
-//binary_to_octal()
-return 0;
+	binary_to_decimal(user_input);
+	//binary_to_octal()
+	return 0;
 
 }
